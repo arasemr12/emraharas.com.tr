989218506965a7e371400e20a1b3051aaa6e05a2
Emrah <arasemr1234@gmail.com>
2025-07-02 15:21:34 +0300

    Update signal.vue

--- pages/signal.vue
+++ pages/signal.vue
@@ -15,22 +15,47 @@ const dvdHeight = 119.5;
 
 const speed = 2;
 
+const colors = [
+    "#be00ff",
+    "#00feff",
+    "#ff8300",
+    "#0026ff",
+    "#fffa01",
+    "#ff2600",
+    "#ff008b",
+    "#25ff01",
+];
+
+const i = ref(0);
+
 const animate = () => {
+    canvas.value.width = window.innerWidth;
+    canvas.value.height = window.innerHeight;
+
+    canvas.value.style.width = "100%";
+    canvas.value.style.height = "100%";
+
     if(x.value+dvdWidth > window.innerWidth){
         xDirection.value = true;
+        i.value++;
     };
 
     if(x.value-1 < 0){
         xDirection.value = false;
+        i.value++;
     };
 
     if(y.value-1 < 0){
         yDirection.value = false;
+        i.value++;
     };
 
     if(y.value+dvdHeight > window.innerHeight){
         yDirection.value = true;
-    }
+        i.value++;
+    };
+
+    if(i.value > 7) i.value = 0;
 
     if(xDirection.value){
         x.value-=speed;
@@ -46,13 +71,11 @@ const animate = () => {
 
     const ctx = canvas.value.getContext("2d");
 
-    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
-    /*ctx.beginPath();
-    ctx.rect(x.value, y.value, 150, 100);
-    ctx.fillStyle = "#fff";
-    ctx.fill();*/
-
+    ctx.fillStyle = colors[i.value];
+    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+    ctx.globalCompositeOperation = "destination-in";
     ctx.drawImage(dvd.value,x.value,y.value,dvdWidth,dvdHeight);
 
     requestAnimationFrame(animate);
@@ -81,6 +104,13 @@ onMounted(() => {
     dvd.value = image;
 
     image.onload = initialize;
+
+    document.addEventListener("keydown",(e) => {
+        if(e.code != "ArrowRight") return;
+        
+        i.value++;
+        if(i.value > 7) i.value = 0;
+    });
 });
 
 </script>
